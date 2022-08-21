3a945f662f56dd2fcf05ce6c1af4af6b43170511
amalraju7 <amalraju443@gmail.com>
2022-08-21 09:32:02 +0530

    Initial Commit

---
+++ Hotdog CoreML/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hotdog CoreML
+//
+//  Created by Amal Raju on 21/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
